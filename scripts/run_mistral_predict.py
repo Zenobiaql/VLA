36bac71402d7a543fadf57b1e90c89c710a83c6f
RedFairy <rundongluo2002@gmail.com>
2024-04-24 21:32:10 +0800

    update

--- scripts/run_mistral_predict.py
+++ scripts/run_mistral_predict.py
@@ -1,80 +1,91 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from alignment import DataArguments, H4ArgumentParser, ModelArguments, SFTConfig
 from alignment import get_VLA_dataset_legacy
-
-from huggingface_hub import login
-login(token='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')
 import torch
 
-device = 'cuda:0'
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_name_or_path', type=str, default='../data-rundong/VLA-experiments/Mistral-7B-8nodes-zero3/checkpoint-32000')
+parser.add_argument('--base_model_name', type=str, default='mistralai/Mistral-7B-v0.1')
+parser.add_argument('--data_root', type=str, default='/mnt/robotdata/bridge2_tokenized_legacy')
+parser.add_argument('--num_visual_tokens', type=int, default=16384)
+parser.add_argument('--num_action_tokens', type=int, default=256)
+parser.add_argument('--num_input_frames', type=int, default=6)
+parser.add_argument('--num_output_frames', type=int, default=1)
+args = parser.parse_args()
 
-model_name_or_path = '/mnt/azureml/cr/j/d8c986f48e0042758991784fe953c9c3/exe/wd/data-rundong/VLA-experiments/Mistral-7B-8nodes-zero3/checkpoint-32000'
-# model_name_or_path = 'mistralai/Mistral-7B-v0.1'
-model = AutoModelForCausalLM.from_pretrained(model_name_or_path)
-tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
+def main():
+	from huggingface_hub import login
+	login(token='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')
 
-vocab_size = len(AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1"))
-dataset = get_VLA_dataset_legacy(vocab_size, split='test')
+	tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
 
-def preprocess_func(example): 
-    '''
-    Format the example into a sequence format
-    examples is a dict with the following keys:
-    - text: text prompt of the manipulation task, in natural language
-        since max sequence length is 2048+256=2304, its max number of tokens (for 4 input visuals + 4 output visuals)
-        2304 - 2 (start&end) - 12 (special tokens) - (4+4)*256 - (4+4)*7 = 186
-        (the begin of sequence token will be automatically added by the mistral tokenizer, but no for llama tokenizer)
-    - task_description: task description in natural language
-    - input_plan_description: input plan description in natural language
-    - output_plan_description: output plan description in natural language
-    - input_visual: input visual tokens for the manipulation task, in token format, e.g., <v1> <v2> <v3>
-    - input_action: input action tokens for the manipulation task, in token format
-    - output_visual: output visual tokens for the manipulation task, in token format
-    - output_action: output action tokens for the manipulation task, in token format
-    sequence format: bos (used for mistral, not used for phi) + bot_i + text + eot_i +
-                    bov_i + input_visual + eov_i +
-                    boa_i + input_action + eoa_i + 
-                    bov_o + output_visual + eov_o +
-                    boa_o + output_action + eoa_o + eos (padding will be automatically added later by the trainer)
-    '''
-    
-    example['text'] = '<bot_i>' + example['task_description'] + example['input_plan_description'] + '<eot_i>' + \
-                '<bov_i>' + ''.join(tokenizer.convert_ids_to_tokens(example['input_visual'])) + '<eov_i>' + \
-                '<boa_i>' + ''.join(tokenizer.convert_ids_to_tokens(example['input_action'])) + '<eoa_i>' + \
-                '<bot_o>' + example['output_plan_description'] + '<eot_o>' + \
-                '<bov_o>' + ''.join(tokenizer.convert_ids_to_tokens(example['output_visual'])) + '<eov_o>' + \
-                '<boa_o>' + ''.join(tokenizer.convert_ids_to_tokens(example['output_action'])) + '<eoa_o>' + \
-                tokenizer.eos_token
+	vocab_size = len(AutoTokenizer.from_pretrained(args.base_model_name))
+	eval_dataset = get_VLA_dataset_legacy(args, vocab_size, split='test')
+	print(len(eval_dataset))
 
-    return example
+	def preprocess_func(example): 
+		'''
+		Format the example into a sequence format
+		examples is a dict with the following keys:
+		- text: text prompt of the manipulation task, in natural language
+			since max sequence length is 2048, its max number of tokens is 2048 - 12 - 6*256 - 6*7 - 256 - 7 = 195
+		- input_visual: input visual tokens for the manipulation task, in token format, e.g., <v1> <v2> <v3>
+		- input_action: input action tokens for the manipulation task, in token format
+		- output_visual: output visual tokens for the manipulation task, in token format
+		- output_action: output action tokens for the manipulation task, in token format
+		sequence format: bos + bot_i + text + eot_i +
+						bov_i + input_visual + eov_i +
+						boa_i + input_action + eoa_i + 
+						bov_o + output_visual + eov_o +
+						boa_o + output_action + eoa_o + eos (padding will be automatically added later by the trainer)
+		'''
+		example['text'] = '<bot_i>' + example['text'] + '<eot_i>' + \
+					'<bov_i>' + ''.join(tokenizer.convert_ids_to_tokens(example['input_visual'])) + '<eov_i>' + \
+					'<boa_i>' + ''.join(tokenizer.convert_ids_to_tokens(example['input_action'])) + '<eoa_i>' + \
+					'<bov_o>' + ''.join(tokenizer.convert_ids_to_tokens(example['output_visual'])) + '<eov_o>' + \
+					'<boa_o>' + ''.join(tokenizer.convert_ids_to_tokens(example['output_action'])) + '<eoa_o>' + \
+					tokenizer.eos_token
 
-train_dataset = train_dataset.map(
-    preprocess_func,
-    num_proc=data_args.preprocessing_num_workers,
-    remove_columns=column_names,
-    desc="Preprocessing training dataset",
-)
-eval_dataset = eval_dataset.map(
-    preprocess_func,
-    num_proc=data_args.preprocessing_num_workers,
-    remove_columns=column_names,
-    desc="Preprocessing testing dataset",
-)
+		return example
 
-# create 6*256 random visual tokens
-random_visual_tokens = torch.randint(0, 16384, (6, 256))
-# convert into <vxx> format
-random_visual_tokens = [f"<v{v}>" for v in random_visual_tokens.flatten().tolist()]
-# create 6*7 random action tokens
-random_action_tokens = torch.randint(0, 256, (6, 7))
-# convert into <avxx> format
-random_action_tokens = [f"<a{v}>" for v in random_action_tokens.flatten().tolist()]
+	eval_dataset = eval_dataset.map(
+		preprocess_func,
+		remove_columns=['input_visual', 'input_action', 'output_visual', 'output_action'],
+		num_proc=12,
+		desc="Preprocessing testing dataset",
+	)
 
-prompt = "<bot_i>My favourite condiment is<eot_i><bov_i>" + "".join(random_visual_tokens) + "<eov_i><boa_i>" + "".join(random_action_tokens) + "<eoa_i>"
+	device = 'cuda:0'
 
-model_inputs = tokenizer([prompt], return_tensors="pt").to(device)
-model.to(device)
+	# sample a prompt from the eval_dataset
+	input_prompt = eval_dataset[0]['text']
+	print(input_prompt)
 
-generated_ids = model.generate(**model_inputs, max_new_tokens=10000, do_sample=True)
-out = tokenizer.batch_decode(generated_ids)[0]
+	model_inputs = tokenizer(input_prompt, return_tensors="pt").to(device)
+	print(model_inputs)
 
-print(out)
+	# model_name_or_path = 'mistralai/Mistral-7B-v0.1'
+	model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path).to(device)
+
+	generated_ids = model.generate(**model_inputs, max_new_tokens=10000, do_sample=True)
+	out = tokenizer.batch_decode(generated_ids)[0]
+
+	print(out)
+
+def generate_random_prompt():
+	# create 6*256 random visual tokens
+	random_visual_tokens = torch.randint(0, 16384, (6, 256))
+	# convert into <vxx> format
+	random_visual_tokens = [f"<v{v}>" for v in random_visual_tokens.flatten().tolist()]
+	# create 6*7 random action tokens
+	random_action_tokens = torch.randint(0, 256, (6, 7))
+	# convert into <avxx> format
+	random_action_tokens = [f"<a{v}>" for v in random_action_tokens.flatten().tolist()]
+
+	prompt = "<bot_i>My favourite condiment is<eot_i><bov_i>" + "".join(random_visual_tokens) + "<eov_i><boa_i>" + "".join(random_action_tokens) + "<eoa_i>"
+
+	return prompt
+
+if __name__ == '__main__':
+	main()
