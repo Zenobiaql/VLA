a54b391d565307e02dc6032d2b29beb807557543
root <root@e1287c01561d4069b83a1faa332c84d80000N4.1yhk1dhplj4uxeqzcfdtgxiqqa.jx.internal.cloudapp.net>
2024-05-24 15:03:15 +0000

    update

--- scripts/run_tats_tokenized.py
+++ scripts/run_tats_tokenized.py
@@ -9,7 +9,6 @@ from transformers import AutoModelForCausalLM, set_seed, MistralModel, PhiModel
 from transformers import TrainerCallback
 from transformers import Phi3Config, Phi3ForCausalLM, LlamaTokenizer
 from transformers import MistralConfig, MistralForCausalLM
-from transformers_modules.phi_3.configuration_phi3 import Phi3Config
 
 sys.path.append('.')
 from src import DataArguments, H4ArgumentParser, ModelArguments, SFTConfig, get_checkpoint, get_datasets
