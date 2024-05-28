b26a5c25d29453928cb0a9c5a5b5e5c3c04ff726
Rundong Luo <rundongluo2002@gmail.com>
2024-05-28 07:58:35 +0000

    update mii implementation

--- scripts/predict_tats_tokenized_mii.py
+++ scripts/predict_tats_tokenized_mii.py
@@ -1,35 +1,23 @@
 """
-Predict
+Predict with deepspeed-mii
 """
 
 import logging
 import random
 import sys
-
-import datasets
-import torch
 import transformers
-from transformers import AutoModelForCausalLM, set_seed, MistralModel, PhiModel
-from transformers import TrainerCallback
 
 sys.path.append('.')
-from src import DataArguments, H4ArgumentParser, ModelArguments, SFTConfig, get_checkpoint, get_datasets
-from src import get_VLA_dataset
+from src import DataArguments, H4ArgumentParser, ModelArguments, get_VLA_dataset
 
-# from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 import os
 import json
 import time
-from transformers.integrations import HfDeepSpeedConfig
-import deepspeed
 import mii
 
 logger = logging.getLogger(__name__)
 
 def main():
-
-    # parser = H4ArgumentParser((ModelArguments, DataArguments, SFTConfig))
-    # model_args, data_args, training_args = parser.parse()
 
     parser = H4ArgumentParser((ModelArguments, DataArguments))
     model_args, data_args = parser.parse()
@@ -77,17 +65,16 @@ def main():
     local_rank = int(os.getenv("LOCAL_RANK", "0"))
     os.makedirs(os.path.dirname(data_args.save_prediction_path), exist_ok=True)
     f = open(data_args.save_prediction_path, 'a')
-    for i in range(10):
-        index = random.randint(0, len(eval_dataset))
-        input_text = eval_dataset[index]['text']
-        print('input_text', input_text)
+
+    for sample in eval_dataset:
+        input_text = sample['text']
+        # print('input_text', input_text)
         
         start_time = time.time()
         
         output = pipe([input_text], max_new_tokens=1024)
-        print(output)
         output_text = output[0].generated_text
-        print('output_text', output_text)
+        # print('output_text', output_text)
 
         print('generate time', time.time() - start_time)
 
@@ -100,21 +87,21 @@ def main():
             ret['input_clip_description'] = input_text.split('<eotp_i>')[0].split('<botp_i>')[-1]
 
             ret['output_clip_description_pred'] = output_text.split('<eotp_o>')[0].split('<botp_o>')[-1]
-            ret['output_clip_description_gt'] = eval_dataset[index]['output'].split('<eotp_o>')[0].split('<botp_o>')[-1]
-            ret['output_clip_description_value_gt'] = eval_dataset[index]['gt_actions']
+            ret['output_clip_description_gt'] = sample['output'].split('<eotp_o>')[0].split('<botp_o>')[-1]
+            ret['output_clip_description_value_gt'] = sample['gt_actions']
 
-            ret['trajectory_id'] = eval_dataset[index]['trajectory_id']
-            ret['view'] = eval_dataset[index]['view']
+            ret['trajectory_id'] = sample['trajectory_id']
+            ret['view'] = sample['view']
 
             ret['identical_token_ratio_video'], ret['identical_token_ratio_action'] = 0, 0
 
             ret['input_video_tokens'] = [int(x[:-1]) for x in input_text.split('<eov_i>')[0].split('<bov_i>')[-1].split('<va') if x != '']
             ret['output_video_tokens_pred'] = [int(x[:-1]) for x in output_text.split('<eov_o>')[0].split('<bov_o>')[-1].split('<va') if x != '']
-            ret['output_video_tokens_gt'] = [int(x[:-1]) for x in eval_dataset[index]['output'].split('<eov_o>')[0].split('<bov_o>')[-1].split('<va') if x != '']
+            ret['output_video_tokens_gt'] = [int(x[:-1]) for x in sample['output'].split('<eov_o>')[0].split('<bov_o>')[-1].split('<va') if x != '']
 
             ret['input_action_tokens'] = [int(x[:-1]) for x in input_text.split('<eoa_i>')[0].split('<boa_i>')[-1].split('<va') if x != '']
             ret['output_action_tokens_pred'] = [int(x[:-1]) for x in output_text.split('<eoa_o>')[0].split('<boa_o>')[-1].split('<va') if x != '']
-            ret['output_action_tokens_gt'] = [int(x[:-1]) for x in eval_dataset[index]['output'].split('<eoa_o>')[0].split('<boa_o>')[-1].split('<va') if x != '']
+            ret['output_action_tokens_gt'] = [int(x[:-1]) for x in sample['output'].split('<eoa_o>')[0].split('<boa_o>')[-1].split('<va') if x != '']
 
             # print the ratio of identical tokens
             num_identical_tokens = 0
@@ -128,8 +115,6 @@ def main():
                     num_identical_tokens += 1
             ret['identical_token_ratio_action'] = num_identical_tokens / len(ret['output_action_tokens_gt'])
 
-            # print('output_text', output_text)
-            # save as jsonl file
             f.write(json.dumps(ret) + '\n')
 
 if __name__ == "__main__":
