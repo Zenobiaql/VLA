7498dcd28cd6b83cd095516c48556a988493e626
Zenobiaql <zhangqilin8879@mail.ustc.edu.cn>
2024-11-05 10:44:38 +0800

    Signed-off-by: Zenobiaql <zhangqilin8879@mail.ustc.edu.cn>

--- src/configs.py
+++ src/configs.py
@@ -165,7 +165,7 @@ class ModelArguments:
         },
     )
     use_peft: bool = field(
-        default=True,
+        default=False,
         metadata={"help": ("Whether to use PEFT or not for training.")},
     )
     lora_r: Optional[int] = field(
