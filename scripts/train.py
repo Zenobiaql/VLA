a1313630228d416b6bf71ee1db61cd45c63e18e1
Zenobiaql <zhangqilin8879@mail.ustc.edu.cn>
2024-11-12 11:58:51 +0800

    restrict test on sft
    Signed-off-by: Zenobiaql <zhangqilin8879@mail.ustc.edu.cn>

--- scripts/train.py
+++ scripts/train.py
@@ -11,7 +11,7 @@ from collections import OrderedDict
 from safetensors import safe_open
 
 sys.path.append('.')
-from src import DataArguments, H4ArgumentParser, ModelArguments, PEFTConfig, get_checkpoint, get_datasets
+from src import DataArguments, H4ArgumentParser, ModelArguments, SFTConfig, get_checkpoint, get_datasets
 from src import get_VLA_dataset
 
 from src import SFTTrainer, DataCollatorForCompletionOnlyLM
